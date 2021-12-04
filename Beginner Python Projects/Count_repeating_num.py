de5294635b8170112c3ecf662999a662ca31ba1e
Naved Adenwala <81867377+naved-adenwala@users.noreply.github.com>
2021-12-04 23:24:00 +0530

    Update Count_repeating_num.py

--- Beginner Python Projects/Count_repeating_num.py
+++ Beginner Python Projects/Count_repeating_num.py
@@ -1,7 +1,8 @@
+#enter multiple bumber
 inp = input("Enter numbers to count the repetation of that number ")
 
 li = list(inp)
-
+#unique
 unique = []
 for l in li:
     if l not in unique:
@@ -9,10 +10,11 @@ for l in li:
 
 
 length = len(unique)
-
+#counting repeating numbers
 for x in range(0,length):
     find = unique[x]
     count = -1
+    #checking if number is repeating or not
     for y in li:
         if y == find:
             count +=1
