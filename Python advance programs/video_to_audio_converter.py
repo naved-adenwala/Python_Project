3b044e78c9a0068666a3dcd109bdea6419243314
Naved Adenwala <81867377+naved-adenwala@users.noreply.github.com>
2021-12-07 23:46:50 +0530

    Update video_to_audio_converter.py

--- Python advance programs/video_to_audio_converter.py
+++ Python advance programs/video_to_audio_converter.py
@@ -1,10 +1,17 @@
+#module
 from tkinter.filedialog import askopenfilename
 import os
 import moviepy.editor
+#ask file name
 f = askopenfilename()
-video = moviepy.editor.VideoFileClip(f)
+#video file name
+video = moviepy.editor.VideoFileClip(f) 
+#video to audio
 audio = video.audio
+#file name
 aud_name = input("Name your audio file ")
+#file path
 ch = input("Enter path to save this file ")
 os.chdir(ch)
+#output
 audio.write_audiofile(f"{aud_name}.mp3")
