10c8ad3a1df07e5ed6021838ee2827f6f81953a1
Naved Adenwala <81867377+naved-adenwala@users.noreply.github.com>
2021-12-07 23:45:24 +0530

    Update Spelling_correction.py

--- Python advance programs/Spelling_correction.py
+++ Python advance programs/Spelling_correction.py
@@ -1,18 +1,19 @@
+#module
 from textblob import TextBlob
 
-from textblob import TextBlob
-
+#multiple input
 inp = input("Enter words seprated by space to check spelling : ")
 words = inp.split()
-
+#variable
 corrected_words = []
-
+#convert
 for word in words:
     corrected_words.append(TextBlob(word))
-
-print("Wrong words",words)
+#input words
+print("Wrong words",words) 
 
 print("Corrected words are : ")
 print("--------------------------------------------")
 for i in corrected_words:
+    #correct words
     print(i.correct())
